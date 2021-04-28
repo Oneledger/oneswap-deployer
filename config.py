b1ea8f1ed94a617c5745dc2e024fce3b02429e9c
DeV1doR <bogdankurinniy.dev1@gmail.com>
2021-04-29 01:04:04 +0300

    Add uniswap deployment

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-FEE_ADDRESS = '1c4d8fd351846b3a337c0cd22f1ee33680db3c50'
+FEE_ADDRESS = os.environ['FEE_ADDRESS']
 DEPLOYER_PK = os.environ['DEPLOYER_PK']
-NODE_URL = 'http://127.0.0.1:26602/jsonrpc'
+NODE_URL = os.environ.get('NODE_URL', 'http://127.0.0.1:26602/jsonrpc')
