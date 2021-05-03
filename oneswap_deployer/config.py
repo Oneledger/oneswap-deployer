2244badc4f452c85d3b58967a025d2bdbd52920c
DeV1doR <bogdankurinniy.dev1@gmail.com>
2021-05-03 06:47:12 +0300

    Add remove liquidity

--- oneswap_deployer/config.py
+++ oneswap_deployer/config.py
@@ -9,3 +9,5 @@ DEPLOYER_PK = os.environ['DEPLOYER_PK']
 NODE_URL = os.environ.get('NODE_URL', 'http://127.0.0.1:26602/jsonrpc')
 STATE_FILE = os.environ.get('STATE_FILE', os.path.join(BASE_DIR, 'db/state.json'))
 SWAP_LIST_FILE = os.environ.get('SWAP_LIST_FILE', os.path.join(BASE_DIR, 'db/default_swap_list.json'))
+
+os.makedirs(os.path.join(BASE_DIR, 'db'), exist_ok=True)
